14248efdbfcc3b06b7af520374ea820ff10cdad8
Temuri Takalandze <takalandzet@gmail.com>
2021-04-19 12:31:15 +0400

    :memo: Add external links to the documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # General information about the project.
-project = u'Drups.io Docs'
+project = u'Drups.io'
 copyright = u'2021, Drups.io'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -184,7 +184,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'DrupsIoDocs.tex', u'Drups.io Docs', u'Drups.io', 'manual'),
+  ('index', 'DrupsIoDocs.tex', u'Drups.io', u'Drups.io', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'drupsiodocs', u'Drups.io Docs', [u'Drups.io'], 1)
+    ('index', 'drupsiodocs', u'Drups.io', [u'Drups.io'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -226,7 +226,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'DrupsIoDocs', u'Drups.io Docs', u'Drups.io', 'DrupsIoDocs', 'Drups.io Docs', 'Development'),
+  ('index', 'DrupsIoDocs', u'Drups.io', u'Drups.io', 'DrupsIoDocs', 'Drups.io', 'Development'),
 ]
 
 # Documents to append as an appendix to all manuals.
--- docs/index.rst
+++ docs/index.rst
@@ -1,7 +1,23 @@
 Drups.io Documentation
 ==================================================
 
-Contents:
+Links
+-----
+
+Documentation
+
+- `Drups.io Engine <https://drupsio.rtfd.io/projects/engine/>`_
+- `Drups.io Application <https://drupsio.rtfd.io/projects/application/>`_
+- `Drups.io Docker <https://drupsio.rtfd.io/projects/docker/>`_
+
+Repositories
+
+- `Drups.io Engine <https://github.com/drupsio/engine/>`_
+- `Drups.io Application <https://github.com/drupsio/application/>`_
+- `Drups.io Docker <https://github.com/drupsio/docker/>`_
+
+Contents
+--------
 
 .. toctree::
    :maxdepth: 3
